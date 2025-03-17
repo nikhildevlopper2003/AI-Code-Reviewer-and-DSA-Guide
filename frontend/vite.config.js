d45ec4f561131bdca37610888de9a4adc54c2d2c
Nikhil Singh <nikhilrghvsingh@gmail.com>
2025-03-17 16:58:26 +0530

    Updated base path for Netlify

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  define: {
-    "process.env.BACKEND_URL": JSON.stringify(
-      "https://ai-code-reviewer-and-dsa-guide.onrender.com"
-    ),
-  },
+  base: "./",
+  plugins: [react()],
 });
